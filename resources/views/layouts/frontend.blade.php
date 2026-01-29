cc72ad86b8fa5b8b5a4b7be6821cf9c9884c17ba
sarbjeet <sarbjeet@gmail.com>
2026-01-29 12:51:53 +0530

    slider

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -32,32 +32,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-        <script>
-            document.addEventListener("DOMContentLoaded", function () {
-                const testiSwiper = new Swiper(".testiSwiper", {
-                loop: true,
-                slidesPerView: 1,
-                spaceBetween: 30,
-                speed: 600,
-
-                navigation: {
-                    nextEl: ".custom-next",
-                    prevEl: ".custom-prev",
-                },
-
-                autoplay: {
-                    delay: 4000,
-                    disableOnInteraction: false,
-                },
-
-                breakpoints: {
-                    1024: {
-                    slidesPerView: 1,
-                    }
-                }
-                });
-            });
-        </script>
+        
 
 
         <!-- Styles / Scripts -->
